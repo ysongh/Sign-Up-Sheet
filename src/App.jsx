a10dd81d5601fc3f0a44fb1cecbc70a41593f124
You Song Hou <ysongweb3@gmail.com>
2023-11-06 01:15:38 -0500

    feat: install and add Chakra UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
+import { ChakraProvider, Text } from '@chakra-ui/react';
 
 import './App.css';
 
 function App() {
 
   return (
-    <>
+    <ChakraProvider>
       <h1>Sign Up Sheet</h1>
-    </>
+      <Text>Test</Text>
+    </ChakraProvider>
   )
 }
 
