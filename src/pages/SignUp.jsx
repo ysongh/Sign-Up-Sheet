1f698fcb2ff3a45566374c47dd71b9ae26429bda
You Song Hou <ysongweb3@gmail.com>
2023-11-22 22:40:23 -0500

    refactor: eliminate the two-dimensional array for the sign-up data

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -2,15 +2,11 @@ import { useState } from 'react';
 import { Container, Box, Heading } from '@chakra-ui/react';
 
 const SignUp = () => {
-  const [data, setData] = useState([
-    ['A1', 'B1', 'C1'],
-    ['A2', 'B2', 'C2'],
-    ['A3', 'B3', 'C3'],
-  ]);
+  const [data, setData] = useState(['A1', 'B1', 'C1']);
 
-  const handleCellChange = (rowIndex, colIndex, newValue) => {
+  const handleCellChange = (index, newValue) => {
     const newData = [...data];
-    newData[rowIndex][colIndex] = newValue;
+    newData[index] = newValue;
     setData(newData);
   };
 
@@ -18,21 +14,19 @@ const SignUp = () => {
     <Container>
       <Heading mb="5">Sign Up</Heading>
       <Box flex>
-        {data.map((row, rowIndex) => (
-          row.map((cell, colIndex) => (
-            <Box
-              key={`cell-${rowIndex}-${colIndex}`}
-              p={2}
-              mb={2}
-              borderBottomWidth="1px"
-              borderRightWidth="1px"
-              contentEditable
-              suppressContentEditableWarning
-              onBlur={(e) => handleCellChange(rowIndex, colIndex, e.target.textContent)}
-            >
-              {cell}
-            </Box>
-          ))
+        {data.map((d, index) => (
+          <Box
+            key={index}
+            p={2}
+            mb={2}
+            borderBottomWidth="1px"
+            borderRightWidth="1px"
+            contentEditable
+            suppressContentEditableWarning
+            onBlur={(e) => handleCellChange(index, e.target.textContent)}
+          >
+            {d}
+          </Box>
         ))}
       </Box>
     </Container>
